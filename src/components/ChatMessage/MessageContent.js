9a7a4440127e92d1738915a78e02998606e72537
quantomistro <mahmoodsaifi38@gmail.com>
2020-08-11 22:41:08 +0800

    more message content for testing

--- src/components/ChatMessage/MessageContent.js
+++ src/components/ChatMessage/MessageContent.js
@@ -6,7 +6,10 @@ class MessageContent extends React.Component {
         return (
             <div className="message-content-style">
                 <h5>Message Author</h5>
-                <p>Message content here</p>
+                <p>
+                    Message content here but the contents of this message span <strong>very</strong> long lol <br/>
+                    they even have these <em>line breaks</em> oof
+                </p>
             </div>
         )
     }
