dee8aee88b9c52df177d88403af88d4cd38af8df
quantomistro <mahmoodsaifi38@gmail.com>
2020-08-11 19:56:14 +0800

    try a different pfp lol

--- src/components/ChatMessage/ProfilePicture.js
+++ src/components/ChatMessage/ProfilePicture.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './ProfilePicture.css';
-import logo from '../../logo.svg';
+import pfp from '../../chip.jpg';
 
 class ProfilePicture extends React.Component {
     render() {
         return (
             <div className="profile-picture-style">
-                <img src={logo} alt="Author's profile icon" />
+                <img src={pfp} alt="Author's profile icon" />
             </div>
         )
     }
